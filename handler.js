accbc72b37edc6409de5a8b080d59b5edf3003b2
Satheesh C <satheesancchandran@gmail.com>
2020-01-30 09:33:22 +0530

    Created a class for deciding the response handlers

---
+++ handler.js
@@ -0,0 +1,140 @@
+const fs = require('fs');
+const { App } = require('./appClass');
+const CONTENT_TYPES = require('./lib/mimeTypes');
+const notFoundCode = 404;
+
+const setPath = function(req) {
+  let path = `${__dirname}/pages${req.url}`;
+  if (path.includes('sources')) {
+    path = `${__dirname}${req.url}`;
+  }
+  if (req.method === 'GET' && req.url === '/') {
+    path = `${__dirname}/pages/home.html`;
+  }
+  return path;
+};
+
+const getCommentDivisions = function(content, reaction) {
+  const [date, time] = new Date(reaction.date).toLocaleString().split(',');
+  return (
+    content +
+    `<tr>
+          <td> ${date} </td>
+          <td> ${time} </td>
+          <td> ${reaction.name} </td>
+          <td> ${reaction.comment} </td>
+        </tr>`
+  );
+};
+
+const getJSONContent = function() {
+  if (!fs.existsSync('./comments.json')) {
+    return [];
+  }
+  let comments = fs.readFileSync('./comments.json', 'utf8');
+  if (comments === '') {
+    return [];
+  }
+  comments = JSON.parse(comments);
+  return comments;
+};
+
+const provideCommentPage = function(path) {
+  let content = fs.readFileSync(path, 'utf8');
+  const allComment = getJSONContent();
+  const fileDivision = allComment.reduce(getCommentDivisions, '');
+  content = content.replace(/___comments___/g, fileDivision);
+  return content;
+};
+
+const serveStaticPage = function(req, res, next) {
+  const path = setPath(req);
+  const stat = fs.existsSync(path) && fs.statSync(path);
+  if (!stat || !stat.isFile()) {
+    return next();
+  }
+  const content = fs.readFileSync(path);
+  const [, type] = path.split('.');
+  responseWriting(content, type, res);
+};
+
+const responseWriting = function(content, type, res) {
+  res.setHeader('Content-Type', CONTENT_TYPES[type]);
+  res.setHeader('Content-Length', content.length);
+  res.end(content);
+};
+
+const serveGuestPage = function(req, res, next) {
+  const path = setPath(req);
+  const stat = fs.existsSync(path) && fs.statSync(path);
+  if (!stat || !stat.isFile()) {
+    return next();
+  }
+  const [, type] = req.url.split('.');
+  const chunk = parseChunk(req.body);
+  storeInputs(chunk);
+  const content = provideCommentPage(path);
+  responseWriting(content, type, res);
+};
+
+const servePreviousGuestBook = function(req, res, next) {
+  const path = setPath(req);
+  const stat = fs.existsSync(path) && fs.statSync(path);
+  if (!stat || !stat.isFile()) {
+    return next();
+  }
+  const [, type] = req.url.split('.');
+  const content = provideCommentPage(path);
+  responseWriting(content, type, res);
+};
+
+const storeInputs = function(body) {
+  const comments = getJSONContent();
+  comments.unshift(body);
+  fs.writeFileSync('./comments.json', JSON.stringify(comments), 'utf8');
+};
+
+const parseChunk = function(chunk) {
+  const data = {};
+  const [name, comment] = chunk.split('&');
+  [name, comment].forEach(element => {
+    let [key, value] = element.split('=');
+    key = key.replace(/\+/g, ' ');
+    key = decodeURIComponent(key);
+    value = value.replace(/\+/g, ' ');
+    value = decodeURIComponent(value);
+    data[key] = value;
+  });
+  data.date = new Date();
+  return data;
+};
+
+const readBody = function(req, res, next) {
+  let data = '';
+  req.on('data', chunk => {
+    data += chunk;
+  });
+  req.on('end', () => {
+    req.body = data;
+    next();
+  });
+};
+
+const notFound = function(req, res) {
+  res.writeHead(notFoundCode);
+  res.end('Not Found');
+};
+
+const app = new App();
+
+app.use(readBody);
+
+app.get('/guestBook.html', servePreviousGuestBook);
+app.get('', serveStaticPage);
+app.post('/guestBook.html', serveGuestPage);
+app.get('', notFound);
+app.post('', notFound);
+
+// console.log(app);
+
+module.exports = { app };
--- server.js
+++ server.js
@@ -1,37 +1,46 @@
 const http = require('http');
-const {
-  serveGuestPage,
-  serveStaticPage,
-  servePreviousGuestBook
-} = require('./handlers');
-
 const port = 8000;
 
-const setPath = function(req) {
-  let path = `${__dirname}/pages${req.url}`;
-  if (path.includes('sources')) {
-    path = `${__dirname}${req.url}`;
-  }
-  if (req.method === 'GET' && req.url === '/') {
-    path = `${__dirname}/pages/home.html`;
-  }
-  return path;
-};
-
-const requestListener = function(req, res) {
-  const path = setPath(req);
-  if (req.method === 'POST') {
-    return serveGuestPage(path, req, res);
-  }
-  if (req.method === 'GET' && path.includes('guestBook.html')) {
-    return servePreviousGuestBook(path, req, res);
-  }
-  serveStaticPage(path, req, res);
-};
+const { app } = require('./handler');
 
 const main = function() {
-  const server = new http.Server(requestListener);
+  const server = new http.Server(app.serve.bind(app));
   server.listen(port);
 };
 
 main();
+
+// const {
+//   serveGuestPage,
+//   serveStaticPage,
+//   servePreviousGuestBook
+// } = require('./handlers');
+
+// const setPath = function(req) {
+//   let path = `${__dirname}/pages${req.url}`;
+//   if (path.includes('sources')) {
+//     path = `${__dirname}${req.url}`;
+//   }
+//   if (req.method === 'GET' && req.url === '/') {
+//     path = `${__dirname}/pages/home.html`;
+//   }
+//   return path;
+// };
+
+// const requestListener = function(req, res) {
+//   const path = setPath(req);
+//   if (req.method === 'POST') {
+//     return serveGuestPage(path, req, res);
+//   }
+//   if (req.method === 'GET' && path.includes('guestBook.html')) {
+//     return servePreviousGuestBook(path, req, res);
+//   }
+//   serveStaticPage(path, req, res);
+// };
+
+// const main = function() {
+//   const server = new http.Server(requestListener);
+//   server.listen(port);
+// };
+
+// main();

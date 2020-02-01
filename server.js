58e0bad8200e819b87edb8ce8cf0a9a4806f5855
Satheesh C <satheesancchandran@gmail.com>
2020-02-01 09:02:05 +0530

    Test added for POST request
    Added comment class. Previous comments in global.
    
    We are reading the comments json file a single time and new comments are
    pushed into the global comment array

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
+// const { readFileSync, writeFileSync, statSync, existsSync } = require('fs');
 const http = require('http');
 const port = 8000;
 
 const { app } = require('./lib/handlers');
+
+// const fileSystem = {
+//   readFileSync: readFileSync,
+//   writeFileSync: writeFileSync,
+//   statSync: statSync,
+//   existsSync: existsSync
+// };
 
 const main = function() {
   const server = new http.Server(app.serve.bind(app));
--- test/appTest.js
+++ test/appTest.js
@@ -1,8 +1,11 @@
 const request = require('supertest');
+const sinon = require('sinon');
+const fs = require('fs');
 const { app } = require('../lib/handlers');
-const STATUS_CODES = { OK: 200, METHOD_NOT_FOUND: 400 };
 
-describe('GET request for home page', function() {
+const STATUS_CODES = { OK: 200, METHOD_NOT_FOUND: 400, REDIRECT: 301 };
+
+describe('GET request for static files', function() {
   it('should give the home page with url /', function(done) {
     request(app.serve.bind(app))
       .get('/')
@@ -18,9 +21,7 @@ describe('GET request for home page', function() {
       .expect(/const hideImage = function()/)
       .expect(STATUS_CODES.OK, done);
   });
-});
 
-describe('GET request for agetatum page', function() {
   it('should give the ageratum page with url /ageratum.html', function(done) {
     request(app.serve.bind(app))
       .get('/ageratum.html')
@@ -28,43 +29,75 @@ describe('GET request for agetatum page', function() {
       .expect(/<strong> Ageratum, /)
       .expect(STATUS_CODES.OK, done);
   });
-});
 
-describe('GET request for abeliophyllum page', function() {
-  it('should give the abeliophyllum page with url /abeliophyllum.html', function(done) {
+  it('should give the abeliophyllum page for url /abeliophyllum.html', done => {
     request(app.serve.bind(app))
       .get('/abeliophyllum.html')
       .expect('Content-Type', 'text/html')
       .expect(/<strong>Abeliophyllum distichum <\/strong>/)
       .expect(STATUS_CODES.OK, done);
   });
-});
 
-describe('GET request for css files', function() {
   it('should give the css content with url /css/styles.css', function(done) {
     request(app.serve.bind(app))
       .get('/css/styles.css')
       .expect('Content-Type', 'text/css')
       .expect(STATUS_CODES.OK, done);
   });
-});
 
-describe('GET request for jpg files', function() {
-  it('should give the jpg content with url /jpg/freshorigins.jpg', function(done) {
+  it('should give the jpg content with url /jpg/freshorigins.jpg', done => {
     request(app.serve.bind(app))
       .get('/jpg/freshorigins.jpg')
       .expect('Content-Type', 'image/jpeg')
       .expect(STATUS_CODES.OK, done);
   });
+
+  it('should give the pdf file when the request is for pdf file', done => {
+    request(app.serve.bind(app))
+      .get('/pdf/Ageratum.pdf')
+      .expect('Content-Type', 'application/pdf')
+      .expect(STATUS_CODES.OK, done);
+  });
+
+  it('should give the notFound page for non existing page GET', done => {
+    request(app.serve.bind(app))
+      .get('/jahfdkjndfkj')
+      .expect('Content-Type', 'text/html')
+      .expect(/404 File Not Found/)
+      .expect(STATUS_CODES.OK, done);
+  });
+});
+
+describe('GET guestBook.html', function() {
+  // it('should give the guest book page for /guestBook.html', done => {
+  //   request(app.serve.bind(app))
+  //     .get('/guestBook.html')
+  //     .expect('Content-Type', 'text/html')
+  //     .expect(STATUS_CODES.OK, done);
+  // });
+  // before(() => {
+  //   fs.existsSync = sinon.fake.returns(false);
+  // });
+  before(() => sinon.replace(fs, 'existsSync', () => false));
+  it('should give the guest page if the json file is not existing', done => {
+    request(app.serve.bind(app))
+      .get('/guestBook.html')
+      .expect('Content-Type', 'text/html')
+      .expect(STATUS_CODES.OK, done);
+  });
+  after(() => sinon.restore());
 });
 
 describe('redirection for commnent POST', function() {
-  it('should redirect to guest page if the requested with POST for comments', function(done) {
+  before(() => sinon.replace(fs, 'writeFileSync', () => {}));
+  it('should redirect to guest page for POST /comments', done => {
     request(app.serve.bind(app))
-      .get('/comments')
-      .expect('Content-Type', 'text/html')
-      .expect(200, done);
+      .post('/comments')
+      .send('name=satheesh&comment=hai+satheesh')
+      .expect('Location', '/guestBook.html')
+      .expect(STATUS_CODES.REDIRECT, done);
   });
+  after(() => sinon.restore());
 });
 
 describe('Not Allowed Method', () => {
@@ -77,11 +110,4 @@ describe('Not Allowed Method', () => {
   });
 });
 
-describe('test of some function', function() {
-  it('should give the pdf file when the request is for pdf file', done => {
-    request(app.serve.bind(app))
-      .get('/pdf/Ageratum.pdf')
-      .expect('Content-Type', 'application/pdf')
-      .expect(STATUS_CODES.OK, done);
-  });
-});
+describe('test of some function', function() {});

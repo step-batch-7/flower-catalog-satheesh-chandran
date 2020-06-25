22faaf5dcad86ef9a1e72d34c1e6b6976fa7c9ac
Satheesh C <satheesancchandran@gmail.com>
2020-06-25 14:43:45 +0530

    Added sqlite3 database to store the comments

---
+++ schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS comments_log
+(
+  name VARCHAR(30),
+  comment VARCHAR(200),
+  date DATE
+);
--- server.js
+++ server.js
@@ -1,10 +1,17 @@
+const sqlite = require('sqlite3');
+const { readFileSync } = require('fs');
+
 const { env } = process;
 
 const port = 8000;
 const PORT = env.PORT || port;
 const { app } = require('./lib/routes');
 
-const main = function() {
+const main = function () {
+  const schema = readFileSync('./schema.sql', 'utf8');
+  const db = new sqlite.Database('data/comments.db');
+  db.run(schema);
+  app.locals.db = db;
   app.listen(PORT, () => process.stdout.write(`Listening at ${PORT}`));
 };
 
